eb0ee9e2b2cdf4a076bf0cb169bb18c4a808d2df
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-03-27 12:47:06 +0000

    chore: now sending tokens in the body

--- packages/backend/handler.ts
+++ packages/backend/handler.ts
@@ -14,35 +14,13 @@ export const handler: Handler = async (
   event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResultV2 | void> => {
   try {
-    const { username, password, redirectUri } = JSON.parse(event.body ?? "");
+    const { username, password } = JSON.parse(event.body ?? "");
 
-    const token = await getAccessTokenFromUserCredentials(username, password);
+    const tokens = await getAccessTokenFromUserCredentials(username, password);
 
     const response = {
-      statusCode: 307,
-      statusDescription: "Temporary Redirect",
-      headers: {
-        location: redirectUri,
-      },
-      multiValueHeaders: {
-        "Set-Cookie": [
-          `accessToken=${
-            token.access_token
-          }; Domain=${redirectUri}; Expires=${getUnixTime(
-            add(new Date(), { seconds: token.expires_in })
-          )}; HttpOnly; Secure`,
-          `refreshToken=${
-            token.refresh_token
-          }; Domain=${redirectUri}; Expires=${getUnixTime(
-            add(new Date(), { seconds: token.refresh_expires_in })
-          )}; HttpOnly; Secure`,
-          `idToken=${
-            token.id_token
-          }; Domain=${redirectUri}; Expires=${getUnixTime(
-            add(new Date(), { seconds: token.id_expires_in })
-          )}; HttpOnly; Secure`,
-        ],
-      },
+      statusCode: 200,
+      body: JSON.stringify({ tokens }),
     };
 
     return response;
