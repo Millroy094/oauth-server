c8d5c3ea3646131461e093a0e8a4bdfd41aeac34
Millroy094 <58091953+Millroy094@users.noreply.github.com>
2024-03-31 23:32:02 +0100

    Update main.tf

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -49,8 +49,9 @@ resource "aws_s3_bucket_policy" "auth_website_bucket_policy" {
         "Version": "2012-10-17",
         "Statement": [
             {
+                "Sid": "PublicReadGetObject",
                 "Action": [
-                    "s3:getObject"
+                    "s3:GetObject"
                 ],
                 "Effect": "Allow",
                 "Resource": "arn:aws:s3:::${random_pet.auth_website_bucket_name.id}/*",
@@ -83,8 +84,8 @@ resource "aws_s3_bucket_acl" "auth_website_bucket_acl" {
 resource "aws_s3_bucket_public_access_block" "lambda_bucket" {
   bucket = aws_s3_bucket.auth_website_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
