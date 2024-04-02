be370e387f1bf04e3599447af1d6f7c2789eb33a
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-02 13:47:51 +0100

    tweak

--- scripts/build-website-and-lambda.sh
+++ scripts/build-website-and-lambda.sh
@@ -7,6 +7,6 @@ BACKEND_SOURCE_DIR="$SOURCE_DIR/packages/backend"
 (cd $SOURCE_DIR && pnpm -r install && pnpm -r run build)
 
 #Prepare backend
-(cp "$BACKEND_SOURCE_DIR/package.json" "$SOURCE_DIR/build")
-(cp "$BACKEND_SOURCE_DIR/node_modules" "$SOURCE_DIR/build")
+(cp "$BACKEND_SOURCE_DIR/package.json" "$BACKEND_SOURCE_DIR/build")
+(cp -r "$BACKEND_SOURCE_DIR/node_modules" "$BACKEND_SOURCE_DIR/build")
 
