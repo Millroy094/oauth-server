fb208dafc9a17d8a2bda07f8c2f9b056244ae893
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-02 14:33:48 +0100

    tweak

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -15,6 +15,12 @@ resource "random_pet" "auth_website_bucket_name" {
 resource "aws_s3_bucket" "auth_website_bucket" {
   bucket        = random_pet.auth_website_bucket_name.id
   force_destroy = true
+
+  acl = "public-read"
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
 }
 
 resource "aws_s3_object" "auth_website_code_s3_object" {
@@ -22,49 +28,4 @@ resource "aws_s3_object" "auth_website_code_s3_object" {
   key    = "auth-website.zip"
   source = data.archive_file.archive_auth_website.output_path
   etag   = filemd5(data.archive_file.archive_auth_website.output_path)
-  acl    = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "auth_website_bucket_policy" {
-  bucket = aws_s3_bucket.auth_website_bucket.id
-  policy = <<EOF
-  {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "PublicReadGetObject",
-                "Action": [
-                  "s3:GetObject",
-                  "s3:PutObject",
-                  "s3:PutBucketAcl",
-                  "s3:PutBucketPolicy"
-                ],
-                "Effect": "Allow",
-                "Resource": [
-                  "arn:aws:s3:::${aws_s3_bucket.auth_website_bucket.bucket}",
-                  "arn:aws:s3:::${aws_s3_bucket.auth_website_bucket.bucket}/*"
-                ],
-                "Principal": "*"
-            }
-        ]
-  }
-  EOF
-}
-
-resource "aws_s3_bucket_website_configuration" "auth_website_configuration" {
-  bucket = aws_s3_bucket.auth_website_bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
-
-}
-
-resource "aws_s3_bucket_acl" "auth_website_bucket_acl" {
-  bucket = aws_s3_bucket.auth_website_bucket.id
-  acl    = "public-read"
 }
