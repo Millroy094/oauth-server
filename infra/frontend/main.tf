9d1840e5f9b1a25d5f51609b1a28fc57f8b6542d
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-02 12:08:55 +0100

    tweak

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -10,7 +10,9 @@ resource "null_resource" "website_package_build" {
   provisioner "local-exec" {
     command = <<EOT
       SOURCE_DIR="${path.module}/../../packages/frontend"
-      (cd $SOURCE_DIR && echo "REACT_APP_API_ENDPOINT=${var.auth_lambda_url}" >> .env && npm ci && npm run build)
+      (cd $SOURCE_DIR && echo "REACT_APP_API_ENDPOINT=${var.auth_lambda_url}" >> .env)
+      (cd $SOURCE_DIR && npm ci && ls)
+      (cd $SOURCE_DIR && npm run build)
     EOT
   }
 
