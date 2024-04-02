1e6da84b7feecd9817dc83e32436cff0b05c3371
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-02 16:22:01 +0100

    tweak

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -9,21 +9,22 @@ resource "aws_s3_bucket" "auth_website_bucket" {
 }
 resource "aws_s3_object" "auth_website_code_s3_object_index" {
   bucket = aws_s3_bucket.auth_website_bucket.id
-  key    = "auth-website-index"
+  key    = "index.html"
   source = "${path.module}/../../packages/frontend/dist/index.html"
 }
 
 resource "aws_s3_object" "auth_website_code_s3_object_logo" {
   bucket = aws_s3_bucket.auth_website_bucket.id
-  key    = "auth-website-logo"
+  key    = "mtech.svg"
   source = "${path.module}/../../packages/frontend/dist/mtech.svg"
 }
-resource "aws_s3_object" "auth_website_code_s3_object_assets" {
+resource "aws_s3_object" "auth_website_code_s3_object_asset" {
   bucket = aws_s3_bucket.auth_website_bucket.id
-  key    = "auth-website-assets"
-  source = "${path.module}/../../packages/frontend/dist/assets"
-}
 
+  for_each = fileset("${path.module}/../../packages/frontend/dist/assets/", "**/*.*")
+  key      = "assets/${each.value}"
+  source   = "${path.module}/../../packages/frontend/dist/assets/${each.value}"
+}
 resource "aws_s3_bucket_acl" "auth_website_bucket_acl" {
   bucket = aws_s3_bucket.auth_website_bucket.id
   acl    = "public-read"
--- scripts/build-website-and-lambda.sh
+++ scripts/build-website-and-lambda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE_DIR="."
-
+``
 BACKEND_SOURCE_DIR="$SOURCE_DIR/packages/backend"
 
 (cd $SOURCE_DIR && pnpm -r install && pnpm -r run build)
