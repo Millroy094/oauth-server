166f97ca0701a5998bd9023c8465073a55660b49
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-02 17:13:58 +0100

    tweak

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -54,10 +54,3 @@ resource "aws_s3_bucket_policy" "auth_website_bucket_policy" {
     }]
   })
 }
-
-resource "aws_s3_account_public_access_block" "auth_website_bucket_public_access_block" {
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
