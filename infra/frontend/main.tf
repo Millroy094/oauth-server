7dc4f226b089763f1182ed4a19f88bd717be952c
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-02 15:49:18 +0100

    tweak

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -6,11 +6,6 @@ resource "random_pet" "auth_website_bucket_name" {
 resource "aws_s3_bucket" "auth_website_bucket" {
   bucket        = random_pet.auth_website_bucket_name.id
   force_destroy = true
-  acl           = "public-read"
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
 }
 resource "aws_s3_object" "auth_website_code_s3_object" {
   bucket = aws_s3_bucket.auth_website_bucket.id
@@ -24,7 +19,6 @@ resource "aws_s3_bucket_acl" "auth_website_bucket_acl" {
 }
 
 resource "aws_s3_bucket_website_configuration" "auth_website_code_s3_configuration" {
-
   bucket = aws_s3_bucket.auth_website_bucket.id
   index_document {
     suffix = "index.html"
