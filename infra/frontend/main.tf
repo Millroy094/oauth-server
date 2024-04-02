d42ec8cc325865cfa002dcad87409205fee038da
Millroy094 <58091953+Millroy094@users.noreply.github.com>
2024-04-02 19:11:07 +0100

    fix: frontend infra fix

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -71,13 +71,13 @@ resource "aws_s3_bucket_policy" "auth_website_bucket_policyv2" {
   })
 }
 
-# resource "aws_s3_bucket_public_access_block" "auth_website_bucket_public_access_block" {
-#   bucket                  = aws_s3_bucket.auth_website_bucket.id
-#   block_public_acls       = false
-#   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
-# }
+resource "aws_s3_bucket_public_access_block" "auth_website_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.auth_website_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
 resource "aws_s3_bucket_ownership_controls" "mybucket" {
   bucket = aws_s3_bucket.auth_website_bucket.id
