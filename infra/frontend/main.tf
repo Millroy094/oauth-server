4224928ffa921405c3ab64ae8ef195cf67d745ed
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-22 16:25:28 +0100

    refactor: s3 website upload changes

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -17,7 +17,7 @@ resource "null_resource" "auth_website_package_build" {
 
       (cd $SOURCE_DIR && pnpm --filter @auth/frontend install && pnpm --filter @auth/frontend run build)
 
-      aws s3 sync "../packages/frontend" s3://${aws_s3_bucket.auth_website_bucket.bucket} --delete --exact-timestamps
+      aws s3 sync "../packages/frontend/dist" s3://${aws_s3_bucket.auth_website_bucket.bucket} --delete --exact-timestamps
 
     EOT
   }
