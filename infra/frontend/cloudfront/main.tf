08c492c669dd03b32e041d7e2715433fca08f42a
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-23 17:26:04 +0100

    refactor: terraform refactor

--- infra/frontend/cloudfront/main.tf
+++ infra/frontend/cloudfront/main.tf
@@ -1,4 +1,65 @@
 
+resource "random_pet" "auth_website_bucket_name" {
+  prefix = "auth-website"
+  length = 2
+}
+
+resource "aws_s3_bucket" "auth_website_bucket" {
+  bucket        = random_pet.auth_website_bucket_name.id
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "auth_website_code_s3_configuration" {
+  bucket = aws_s3_bucket.auth_website_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "auth_website_bucket_policy" {
+
+  depends_on = [aws_s3_bucket_acl.auth_website_bucket_acl]
+
+  bucket = aws_s3_bucket.auth_website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "PublicReadGetObject"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.auth_website_bucket.arn}/*",
+    }]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "auth_website_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.auth_website_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "auth_website_ownership_controls" {
+  bucket = aws_s3_bucket.auth_website_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "auth_website_bucket_acl" {
+
+  depends_on = [aws_s3_bucket_ownership_controls.auth_website_ownership_controls, aws_s3_bucket_public_access_block.auth_website_bucket_public_access_block]
+
+  bucket = aws_s3_bucket.auth_website_bucket.id
+  acl    = "public-read"
+}
 
 resource "aws_cloudfront_origin_access_identity" "auth_oai" {
   comment = "auth-website OAI"
@@ -6,7 +67,7 @@ resource "aws_cloudfront_origin_access_identity" "auth_oai" {
 
 resource "aws_cloudfront_distribution" "auth_distribution" {
   origin {
-    domain_name = var.auth_website_bucket.bucket_regional_domain_name
+    domain_name = aws_s3_bucket.auth_website_bucket.bucket_regional_domain_name
     origin_id   = "auth-origin"
 
     s3_origin_config {
