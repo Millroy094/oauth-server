f462f313fafe2974966fa9f57dbd887dc5145fe5
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-23 16:20:24 +0100

    fix: cors for api gateway

--- packages/frontend/src/api/login.ts
+++ packages/frontend/src/api/login.ts
@@ -2,11 +2,15 @@ import axios from "axios";
 
 const login = async (username: string, password: string): Promise<void> => {
   try {
-    console.log(import.meta.env)
-    const response = await axios.post(import.meta.env.VITE_AUTH_API_ENDPOINT, {
-      username,
-      password,
-    });
+    console.log(import.meta.env);
+    const response = await axios.post(
+      import.meta.env.VITE_AUTH_API_ENDPOINT,
+      {
+        username,
+        password,
+      },
+      { withCredentials: true }
+    );
     console.log(response);
   } catch (err) {
     console.log("Login was not successful", err);
