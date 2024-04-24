13870f4c2bcba569983d69b33ea036605e640223
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-24 15:26:52 +0100

    fix: cors issues

--- packages/backend/handler.ts
+++ packages/backend/handler.ts
@@ -12,6 +12,22 @@ export const handler: Handler = async (
   event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResultV2 | void> => {
   console.log(event);
+
+  if (event.requestContext.http.method === "OPTIONS") {
+    // Respond to OPTIONS preflight request
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin":
+          "https://${var.react_auth_website_endpoint}",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: "",
+    };
+    return response;
+  }
+
   try {
     const { username, password } = JSON.parse(event.body ?? "");
 
