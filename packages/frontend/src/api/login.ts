b353e693c6f1f8eccefa56ec09ad9d7faa90dd55
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-24 10:13:42 +0100

    fix: cors issues

--- packages/frontend/src/api/login.ts
+++ packages/frontend/src/api/login.ts
@@ -3,10 +3,14 @@ import axios from "axios";
 const login = async (username: string, password: string): Promise<void> => {
   try {
     console.log(import.meta.env);
-    const response = await axios.post(import.meta.env.VITE_AUTH_API_ENDPOINT, {
-      username,
-      password,
-    });
+    const response = await axios.post(
+      import.meta.env.VITE_AUTH_API_ENDPOINT,
+      {
+        username,
+        password,
+      },
+      { withCredentials: true }
+    );
     console.log(response);
   } catch (err) {
     console.log("Login was not successful", err);
