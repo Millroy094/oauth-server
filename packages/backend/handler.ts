e2570b2c57f779b660114fda3ac3d8e7c2ed98b2
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-04-25 13:23:54 +0100

    feat: added redirect from auth lambda

--- packages/backend/handler.ts
+++ packages/backend/handler.ts
@@ -1,5 +1,6 @@
 import {
   Handler,
+  Context,
   APIGatewayProxyEventV2,
   APIGatewayProxyResultV2,
 } from "aws-lambda";
@@ -9,7 +10,8 @@ import { getAccessTokenFromUserCredentials } from "./utils/keycloak.js";
 dotenv.config();
 
 export const handler: Handler = async (
-  event: APIGatewayProxyEventV2
+  event: APIGatewayProxyEventV2,
+  context: Context
 ): Promise<APIGatewayProxyResultV2 | void> => {
   try {
     const { username, password } = JSON.parse(event.body ?? "");
@@ -28,20 +30,22 @@ export const handler: Handler = async (
     //   },
     // };
 
-    const response = {
-      statusCode: 307,
-      statusDescription: "Temporary Redirect",
-      headers: {
-        location: "https://jwt.io",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Credentials": true,
-      },
-      cookies: [],
-    };
+    // const response = {
+    //   statusCode: 307,
+    //   statusDescription: "Temporary Redirect",
+    //   headers: {
+    //     location: "https://jwt.io",
+    //     "Access-Control-Allow-Origin": "*",
+    //     "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+    //     "Access-Control-Allow-Headers": "Content-Type",
+    //     "Access-Control-Allow-Credentials": true,
+    //   },
+    //   cookies: [],
+    // };
 
-    return response;
+    context.succeed({ location: "https://jwt.io" });
+
+    // return response;
   } catch (err: any) {
     console.log(err);
     return {
