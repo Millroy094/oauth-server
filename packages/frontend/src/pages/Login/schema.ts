ec6cb4460ef17de420bb3ed60262b1450de9692b
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-09-17 13:57:34 +0100

    fix: mfa validation update

--- packages/frontend/src/pages/Login/schema.ts
+++ packages/frontend/src/pages/Login/schema.ts
@@ -1,15 +1,14 @@
-import * as yup from 'yup';
+import * as yup from "yup";
 
 const schema = yup
   .object({
     email: yup.string().email().required(),
     password: yup.string().required(),
     mfaType: yup.string(),
-    otp: yup
-      .string()
-      .when('mfaType', (mfaType) =>
-        mfaType ? yup.string().required().min(6) : yup.string(),
-      ),
+    otp: yup.string().when("mfaType", (mfaType, schema) => {
+      const [type] = mfaType;
+      return type ? schema.required().min(6) : schema;
+    }),
   })
   .required();
 
