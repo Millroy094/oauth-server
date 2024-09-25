a4220f65f7c190ab1d06b0fa982c6f8517ccb055
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-09-25 16:35:56 +0100

    feat: added disabled edit and delete for super admin user

--- packages/frontend/src/pages/Account/Users/index.tsx
+++ packages/frontend/src/pages/Account/Users/index.tsx
@@ -1,4 +1,4 @@
-import { FC, startTransition, useEffect, useState } from 'react';
+import { FC, startTransition, useEffect, useState } from "react";
 import {
   Card,
   CardContent,
@@ -6,25 +6,28 @@ import {
   Grid,
   IconButton,
   Tooltip,
-} from '@mui/material';
-import { DataGrid, GridColDef, GridRowParams } from '@mui/x-data-grid';
-import getUsers from '../../../api/get-users';
-import useFeedback from '../../../hooks/useFeedback';
-import deleteUser from '../../../api/delete-user';
-import { ClearAll, PersonRemove } from '@mui/icons-material';
-import clearUserSessions from '../../../api/clear-user-sessions';
-import UserPopup from './UserPopup';
+} from "@mui/material";
+import { DataGrid, GridColDef, GridRowParams } from "@mui/x-data-grid";
+import getUsers from "../../../api/get-users";
+import useFeedback from "../../../hooks/useFeedback";
+import deleteUser from "../../../api/delete-user";
+import { ClearAll, PersonRemove } from "@mui/icons-material";
+import clearUserSessions from "../../../api/clear-user-sessions";
+import UserPopup from "./UserPopup";
+import { ADMIN_EMAIL } from "../../../constants";
 
 interface User {
   id: string;
   firstName: string;
   lastName: string;
+  email: string;
+  mobile: string;
 }
 
 const Users: FC = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [open, setOpen] = useState(false);
-  const [selectedUserId, setSelectedUserId] = useState<string>('');
+  const [selectedUserId, setSelectedUserId] = useState<string>("");
   const { feedbackAxiosError, feebackAxiosResponse } = useFeedback();
 
   const fetchUsers = async () => {
@@ -34,7 +37,7 @@ const Users: FC = () => {
     } catch (err) {
       feedbackAxiosError(
         err,
-        'There was an issue retreiving users, please try again',
+        "There was an issue retreiving users, please try again"
       );
     }
   };
@@ -46,12 +49,12 @@ const Users: FC = () => {
   const handleDelete = async (id: string): Promise<void> => {
     try {
       const response = await deleteUser(id);
-      feebackAxiosResponse(response, 'Successfully deleted user', 'success');
+      feebackAxiosResponse(response, "Successfully deleted user", "success");
       setUsers(users.filter((user) => user.id !== id));
     } catch (err) {
       feedbackAxiosError(
         err,
-        'There was an issue deleting the user, please try again',
+        "There was an issue deleting the user, please try again"
       );
     }
   };
@@ -61,98 +64,107 @@ const Users: FC = () => {
       const response = await clearUserSessions(id);
       feebackAxiosResponse(
         response,
-        'Successfully deleted user sessions',
-        'success',
+        "Successfully deleted user sessions",
+        "success"
       );
     } catch (err) {
       feedbackAxiosError(
         err,
-        'There was an issue deleting the user sessions, please try again',
+        "There was an issue deleting the user sessions, please try again"
       );
     }
   };
 
   const handleRowClick = (params: GridRowParams) => {
-    const { id } = params;
-    startTransition(() => {
-      setSelectedUserId(id as string);
-      setOpen(true);
-    });
+    const { id, row } = params;
+    const isAdmin = row?.email === ADMIN_EMAIL;
+
+    if (!isAdmin) {
+      startTransition(() => {
+        setSelectedUserId(id as string);
+        setOpen(true);
+      });
+    }
   };
 
   const onClose = () => {
     setOpen(false);
-    setSelectedUserId('');
+    setSelectedUserId("");
     fetchUsers();
   };
 
   const columns: GridColDef<(typeof users)[number]>[] = [
     {
-      field: 'firstName',
-      headerName: 'First Name',
+      field: "firstName",
+      headerName: "First Name",
       width: 180,
       editable: false,
     },
     {
-      field: 'lastName',
-      headerName: 'Last Name',
+      field: "lastName",
+      headerName: "Last Name",
       width: 180,
       editable: false,
     },
     {
-      field: 'email',
-      headerName: 'Email',
+      field: "email",
+      headerName: "Email",
       width: 200,
       editable: false,
     },
     {
-      field: 'mobile',
-      headerName: 'Mobile',
+      field: "mobile",
+      headerName: "Mobile",
       width: 150,
       editable: false,
     },
     {
-      field: 'id',
-      headerName: '',
+      field: "id",
+      headerName: "",
       width: 100,
       editable: false,
       sortable: false,
-      renderCell: (params) => (
-        <Grid container alignContent='center'>
-          <Grid item>
-            <Tooltip title='Clear all sessions'>
-              <IconButton
-                color='error'
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleDeleteSessions(params.value);
-                }}
-              >
-                <ClearAll />
-              </IconButton>
-            </Tooltip>
+      renderCell: (params) => {
+        const isAdmin = params?.row?.email === ADMIN_EMAIL;
+        return (
+          <Grid container alignContent="center">
+            <Grid item>
+              <Tooltip title="Clear all sessions">
+                <IconButton
+                  disabled={isAdmin}
+                  color="error"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleDeleteSessions(params.value);
+                  }}
+                >
+                  <ClearAll />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+            <Grid item>
+              <Tooltip title="Delete user">
+                <IconButton
+                  disabled={isAdmin}
+                  color="error"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleDelete(params.value);
+                  }}
+                >
+                  <PersonRemove />
+                </IconButton>
+              </Tooltip>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Tooltip title='Delete user'>
-              <IconButton
-                color='error'
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleDelete(params.value);
-                }}
-              >
-                <PersonRemove />
-              </IconButton>
-            </Tooltip>
-          </Grid>
-        </Grid>
-      ),
+        );
+      },
     },
   ];
 
   return (
     <Card elevation={0}>
-      <CardHeader title='Users' />
+      <CardHeader title="Users" />
       <CardContent>
         <DataGrid
           rows={users}
