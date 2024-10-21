7ee956245090250bb1047e44b43fc1eb6c254114
Millroy094 <58091953+Millroy094@users.noreply.github.com>
2024-10-21 17:02:13 +0100

    tweaks

--- infra/modules/dynamo-db/main.tf
+++ infra/modules/dynamo-db/main.tf
@@ -22,18 +22,6 @@ resource "aws_dynamodb_table" "client_dynamodb_table" {
   }
 }
 
-resource "aws_dynamodb_table" "client_dynamodb_table" {
-  name           = "Client"
-  hash_key       = "id"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-  attribute {
-    name = "id"
-    type = "S"
-  }
-}
-
 
 resource "aws_dynamodb_table" "oidc_store_dynamodb_table" {
   name           = "OIDCStore"
