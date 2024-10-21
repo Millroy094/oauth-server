1ee001aab9cc8a11fe6486c79d3c50bd3f394a0b
Millroy094 <58091953+Millroy094@users.noreply.github.com>
2024-10-21 23:16:02 +0100

    test

--- infra/main.tf
+++ infra/main.tf
@@ -12,9 +12,9 @@ module "dynamodb" {
   source = "./modules/dynamo-db"
 }
 
-module "eks" {
-  source              = "./modules/eks"
-  vpc_id              = module.vpc.vpc_id
-  private_subnet_ids  = module.vpc.private_subnet_ids
-  dynamodb_table_arns = module.dynamodb.*
-}
+# module "eks" {
+#   source              = "./modules/eks"
+#   vpc_id              = module.vpc.vpc_id
+#   private_subnet_ids  = module.vpc.private_subnet_ids
+#   dynamodb_table_arns = module.dynamodb.*
+# }
