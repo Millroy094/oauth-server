4d71cba88eace80244a787407e20302af80979d9
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-10-22 12:27:54 +0100

    test

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -162,20 +162,20 @@ resource "helm_release" "oauth_lb" {
     value = var.vpc_id
   }
 
-  # set {
-  #   name  = "image.repository"
-  #   value = "602401143452.dkr.ecr.eu-west-2.amazonaws.com/amazon/aws-load-balancer-controller"
-  # }
+  set {
+    name  = "image.repository"
+    value = "602401143452.dkr.ecr.eu-west-2.amazonaws.com/amazon/aws-load-balancer-controller"
+  }
 
-  # set {
-  #   name  = "serviceAccount.create"
-  #   value = "false"
-  # }
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
 
-  # set {
-  #   name  = "serviceAccount.name"
-  #   value = "aws-load-balancer-controller"
-  # }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
 
   # set {
   #   name  = "clusterName"
