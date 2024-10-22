3d7093287a51d4b2346cdf58b86054f5117c1e93
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-10-22 15:18:47 +0100

    test

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -110,7 +110,7 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1alpha1"
     args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
     command     = "aws"
   }
@@ -121,7 +121,7 @@ provider "helm" {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
+      api_version = "client.authentication.k8s.io/v1alpha1"
       args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
       command     = "aws"
     }
