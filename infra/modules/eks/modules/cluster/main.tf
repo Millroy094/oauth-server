f53fa619594f2f9f256761a24ec8de53a55f0ceb
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-10-24 11:08:24 +0100

    feat: added access entry to cluster

--- infra/modules/eks/modules/cluster/main.tf
+++ infra/modules/eks/modules/cluster/main.tf
@@ -6,6 +6,11 @@ resource "aws_eks_cluster" "oauth_server_eks_cluster" {
     subnet_ids = concat(var.private_subnet_ids, var.public_subnet_ids)
   }
 
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   depends_on = [aws_iam_role_policy_attachment.oauth_server_eks_cluster_policy]
 }
 
@@ -20,3 +25,19 @@ resource "aws_iam_openid_connect_provider" "oauth_server_eks_oidc_provider" {
   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
   url             = aws_eks_cluster.oauth_server_eks_cluster.identity[0].oidc[0].issuer
 }
+
+resource "aws_eks_access_entry" "oauth_server_admin_access_entry" {
+  cluster_name  = aws_eks_cluster.oauth_server_eks_cluster.name
+  principal_arn = aws_iam_role.oauth_server_eks_cluster_access_entry_admin_role
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "oauth_server_admin_access_entry_policy_association" {
+  cluster_name  = aws_eks_cluster.oauth_server_eks_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = aws_iam_role.oauth_server_eks_cluster_access_entry_admin_role
+  access_scope {
+    type = "cluster"
+  }
+}
+
