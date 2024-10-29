c787e030211ed1d5fd28c9f6397f5f494b98a6ca
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-10-29 10:53:30 +0000

    feat: added fixture

---
+++ tests/fixtures.ts
@@ -0,0 +1,29 @@
+import { test as base } from '@playwright/test';
+import { MailSlurp } from 'mailslurp-client';
+
+const apiKey = process.env.MAILSLURP_API_KEY!;
+
+export const test = base.extend <{}, {
+  mailslurp: MailSlurp;
+  inbox: { id: string; emailAddress: string, password: string };
+}>({
+  // Fixture to initialize MailSlurp
+  mailslurp: [async ({}, use) => {
+    const mailslurp = new MailSlurp({ apiKey });
+    await use(mailslurp);
+  }, { scope: 'worker' }],
+
+  // Fixture to create a single shared inbox for the entire test file
+  inbox: [async ({ mailslurp }, use) => {
+    // Create an inbox before all tests
+    const { id, emailAddress } = await mailslurp.createInbox();
+    const password = "Password123!"  
+    await use({ id, emailAddress, password });
+
+    // Cleanup: Delete inbox after all tests complete
+    await mailslurp.deleteInbox(id);
+  }, { scope: 'worker' }] 
+});
+
+// Ensure you export 'expect' from Playwright
+export { expect } from '@playwright/test';
--- tests/tests/user-journey.spec.ts
+++ tests/tests/user-journey.spec.ts
@@ -1,21 +1,30 @@
-import { test, expect } from '@playwright/test';
-import { MailSlurp } from 'mailslurp-client';
+import { test, expect } from '../fixtures';
 
 test.describe('User registration', async () => { 
 
-    const apiKey = process.env.MAILSLURP_API_KEY!
-    const mailslurp = new MailSlurp({ apiKey });
-    
-    test.beforeEach(async ({page}) => {
-    await page.goto('/registration');
-    })
+    test.describe.configure({ mode: 'serial' });
 
-    test('can navigate back to login page', async ({ page }) => {
-    await page.getByText('Click here').click()
-    await expect(page).toHaveURL("/login");
+    test.beforeEach(async({page}) => {
+        page.goto("/")
+    })
+    
+    test('Redirected to login', async ({ page }) => {
+        await expect(page).toHaveURL("/login");
+    });
+
+    test('Navigate to registration page and back to login page correctly', async ({ page }) => {
+        await page.getByText('Click here').click()
+        await expect(page).toHaveURL("/registration");
+
+        await page.getByText('Click here').click()
+        await expect(page).toHaveURL("/login");
     });
 
     test('Has all the fields', async ({ page }) => {
+
+        await page.getByText('Click here').click()
+        await expect(page).toHaveURL("/registration");
+
         await expect(page.locator('[name="firstName"]')).toBeVisible();
         await expect(page.locator('[name="lastName"]')).toBeVisible();
         await expect(page.locator('[name="email"]')).toBeVisible();
@@ -25,6 +34,10 @@ test.describe('User registration', async () => {
     });
 
     test('Errors when fields are not filled out correctly', async ({ page }) => {
+
+        await page.getByText('Click here').click()
+        await expect(page).toHaveURL("/registration");
+
         await page.getByRole('button', { name: 'REGISTER' }).click();
 
         await expect(page.getByText('firstName is a required field')).toBeVisible()
@@ -33,31 +46,32 @@ test.describe('User registration', async () => {
         await expect(page.getByText('Please enter a valid number')).toBeVisible()
     });
 
-    test('Successfully registers an user & login for the first time', async ({ page }) => {
+    test('Successfully registers an user & login for the first time', async ({ page, inbox, mailslurp }) => {
 
-        const { id, emailAddress } = await mailslurp.createInbox();
-        const password = 'Password123!'
+        await page.getByText('Click here').click()
+        await expect(page).toHaveURL("/registration");
 
+        
         await page.locator('[name="firstName"]').fill('test')
         await page.locator('[name="lastName"]').fill('test')
-        await page.locator('[name="email"]').fill(emailAddress)
-        await page.locator('[name="password"]').fill(password)
-        await page.locator('[name="confirmPassword"]').fill(password)
+        await page.locator('[name="email"]').fill(inbox.emailAddress)
+        await page.locator('[name="password"]').fill(inbox.password)
+        await page.locator('[name="confirmPassword"]').fill(inbox.password)
         await page.getByPlaceholder('Phone number').fill('447809211141')
         
         await page.getByRole('button', { name: 'REGISTER' }).click();
         await expect(page.getByText('Successfully registered user!')).toBeVisible()
 
         
-        await page.locator('[name="email"]').fill(emailAddress)
+        await page.locator('[name="email"]').fill(inbox.emailAddress)
         await page.getByRole('button', { name: 'NEXT' }).click();
 
-        await page.locator('[name="password"]').fill(password)
+        await page.locator('[name="password"]').fill(inbox.password)
         await page.getByRole('button', { name: 'NEXT' }).click();
 
-        const email = await mailslurp.waitForLatestEmail(id)
+        const email = await mailslurp.waitForLatestEmail(inbox.id)
         const [otp] = /(\d{6})/.exec(email.body!)?? [''];
-        
+
         otp.split('').forEach((element, key) => {
             page.locator(`[aria-label='Please enter OTP character ${key + 1}']`).fill(element)
         });
@@ -71,6 +85,54 @@ test.describe('User registration', async () => {
         await page.getByRole('button', { name: 'LOG OUT' }).click();
         await expect(page).toHaveURL("/login");
 
+    });
+
+    test("Setup MFA", async ({page, inbox, mailslurp}) => {
+        
+        await page.locator('[name="email"]').fill(inbox.emailAddress)
+        await page.getByRole('button', { name: 'NEXT' }).click();
+
+        await page.locator('[name="password"]').fill(inbox.password)
+        await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+
+        await expect(page).toHaveURL("/account");
+
+        await page.getByRole('tab', { name: 'Security' }).click()
+
+        await page.getByRole('button', { name: 'SETUP MFA' }).nth(2).click()
+        
+        await page.getByLabel('Email').fill(inbox.emailAddress)
+        await page.getByRole('button', { name: 'Setup', exact: true }).click()
+        
+        const email = await mailslurp.waitForLatestEmail(inbox.id)
+        const [otp] = /(\d{6})/.exec(email.body!)?? [''];
+
+        otp.split('').forEach((element, key) => {
+            page.locator(`[aria-label='Please enter OTP character ${key + 1}']`).fill(element)
+        });
+
+        await page.getByRole('button', { name: 'Verify', exact: true }).click()
+        await page.getByRole('button', { name: 'LOG OUT' }).click();
+    })
+
+    test("Login with MFA", async ({ page, inbox, mailslurp }) => {
+        await page.locator('[name="email"]').fill(inbox.emailAddress)
+        await page.getByRole('button', { name: 'NEXT' }).click();
+
+        await page.locator('[name="password"]').fill(inbox.password)
+        await page.getByRole('button', { name: 'NEXT' }).click();
+
+        const email = await mailslurp.waitForLatestEmail(inbox.id)
+        const [otp] = /(\d{6})/.exec(email.body!)?? [''];
+
+        otp.split('').forEach((element, key) => {
+            page.locator(`[aria-label='Please enter OTP character ${key + 1}']`).fill(element)
+        });
+
+        await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+        await expect(page).toHaveURL("/account");
+
+        await page.getByRole('button', { name: 'LOG OUT' }).click();
     });
 
 })
