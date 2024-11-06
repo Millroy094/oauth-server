fe82ef6267bd66d8b5aafeb9fccf01811a3e55d3
Millroy Fernandes <millroy098@hotmail.co.uk>
2024-11-06 12:36:19 +0000

    chore: added more test coverage

--- packages/backend/middleware/error-handler.ts
+++ packages/backend/middleware/error-handler.ts
@@ -1,14 +1,9 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import logger from '../utils/logger.ts';
 import HTTP_STATUSES from '../constants/http-status.ts';
 
-const errorHandler = (
-  err: Error,
-  _req: Request,
-  res: Response,
-  _next: NextFunction,
-) => {
-  logger.error((err as Error).message);
+const errorHandler = (err: Error, _req: Request, res: Response) => {
+  logger.error(err.message);
   res
     .status(HTTP_STATUSES.serverError)
     .send({ message: 'There was an issue serving this request' });
